e94d016de5c978c6a0189bed0adb12003f227928
Akash Kasar <akashkasar@email.com>
2026-07-23 06:12:01 -0400

    Updated design for Website

---
+++ css/js/script.js
@@ -0,0 +1,13 @@
+console.log("UI Redesign Loaded Successfully");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(btn=>{
+
+btn.addEventListener("click",()=>{
+
+alert("Feature Coming Soon!");
+
+});
+
+});
